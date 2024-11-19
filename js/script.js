a957ee0015e6316ac2afb730d01f9970f89af196
MrVludo <vladimir.osmekhin@gmail.com>
2024-11-19 16:52:02 +0200

    Added the pages. Copied them from previous site. +Footer

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ document.addEventListener("DOMContentLoaded", () => {
         sidebar.classList.toggle("active");
     });
 });
+
+const is_html = '.html';
+
+const links_to_pages = [
+    `<a href="index${is_html}">ETUSIVU</a>`,
+    `<a href="galleria${is_html}">GALLERIA</a>`,
+    `<a href="tarinat${is_html}">TARINAT</a>`,
+    `<a href="sarjakuvat${is_html}">SARJAKUVAT</a>`,
+]
+
+const page_list = document.getElementById('page_list');
+
+links_to_pages.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = item; 
+    page_list.appendChild(listItem);
+});
